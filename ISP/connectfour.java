c3a2a1c77e1afeeff3547414f7e57eea262f0624
dala1a <0jeonyunseo1107@gmail.com>
2024-06-19 21:02:57 -0400

    keke

--- ISP/connectfour.java
+++ ISP/connectfour.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class connectfour {
     public static void main(String[] args) {
         Frame game = new Frame();
-        ImageIcon logo = new ImageIcon("four.png");
+        ImageIcon logo = new ImageIcon("Logo.png");
         game.setIconImage(logo.getImage());
         game.setSize(700, 900); // Set initial window size
         game.setResizable(false);
